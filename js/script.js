1500f2042052b69055281841c85d954e9349031a
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2025-01-15 10:54:18 +0000

    theme change & light/ dark mode added

--- js/script.js
+++ js/script.js
@@ -14,3 +14,18 @@ pets.addEventListener("mouseover", function () {
     pets.innerText = "20K";
 });
 
+var toggle = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var heading = document.querySelector("h1");
+var theme = document.querySelector("body");
+
+toggle.addEventListener("click", function () {
+    if (theme.classList.contains("light")) {
+        theme.classList.remove("light");
+        ball.classList.remove("move-right");
+        heading.innerText = "Stealth Quincy";
+} else {theme.classList.add("light");
+        ball.classList.add("move-right");
+        heading.innerText = "Party Quincy";
+}
+    });
